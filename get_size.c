5e4273c38c5cd9e74693e6b4fcd464772baeda38
Chrysanthe <eloi.chrysanthe@gmail.com>
2022-04-19 23:35:58 +0200

    fixing some

---
+++ get_size.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_size: will get the size to cast the argument
+ * @format: formated string
+ * @i: List of argumrnts to be printed
+ *
+ * Return: size
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i =  
