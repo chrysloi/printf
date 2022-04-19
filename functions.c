8017949e9687f6c7c2e710c21c4e6955e99e2996
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-04-19 02:41:36 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -94,3 +94,14 @@ return (write(1, str, length));
  * Return: Number of chars printed
  */
 
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+UNUSED(types);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+return (write(1, "%%", 1));
+}
+
