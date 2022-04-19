81536add17c8b2dd962b36d6a153623e144d397c
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-04-19 02:50:57 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -105,3 +105,15 @@ UNUSED(size);
 return (write(1, "%%", 1));
 }
 
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+
