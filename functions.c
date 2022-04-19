58967a8bafdf0550de19657a965d5d2ebe2adb5d
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-04-19 02:11:43 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -4,3 +4,11 @@
  * print char - Prints a char
  * @types: List of arguments
  * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: Width
+ * @precision: Precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed
+ */
+
