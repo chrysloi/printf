e70a1204f492d1ec7cf4dc051bdd76406a498b2e
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-04-19 02:20:07 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -30,3 +30,9 @@ return (handle_write_char(c, buffer, flags, width, precision, size));
  *
  * Return: Number of chars printed
  */
+
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+int length = 0, i;
+char *str = va_arg(types, char*);
+
