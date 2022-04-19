acd984cc3493f0c869659062546c6c8189ce36b8
Chrysanthe <eloi.chrysanthe@gmail.com>
2022-04-19 14:40:39 +0200

    fixing printf

--- printf.c
+++ printf.c
@@ -4,52 +4,50 @@ void print_buff(char buffer[], int *buff_ind);
 
 /**
  * _printf - Prints everything
- * 
- * @format format
+ * @format: format
  * Return: printed char.
  */
 int _printf(const char *format, ...)
 {
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list args;
-    char buffer[BUFF_SIZE];
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list args;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return(-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i != '%'])
-        {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buff(buffer, &buff_ind);
-            printed_chars++;
-        }
-        else
-        {
-            print_buff(buffer, &buff_ind);
-            flags = get_flags(format,&i);
-            width = get_width(format, &i, args);
-            precision = get_precision(format, &i, args);
-            size = get_size(format, &i);
-            i++;
-            printed = handle_print(format, &i, args, buffer, 
-                                    flags, width, precision, size);
-            if (printed == -1)
-                return(-1);
-            printed_chars += printed;
-        }
-    }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i != '%'])
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buff(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buff(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			i++;
+			printed = handle_print(format, &i, args, buffer,
+						flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+	print_buff(buffer, buff_ind);
 
-    print_buff(buffer, buff_ind);
+	va_end(args);
 
-    va_end(args);
-
-    return (printed_chars);
+	return (printed_chars);
 }
 
 /**
@@ -59,7 +57,5 @@ int _printf(const char *format, ...)
  */
 void print_buff(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
-        buff_ind = 0;
+
 }
