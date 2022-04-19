68298343ecb6f1b8e747e18552c7ba04dd5a8464
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-04-19 02:15:13 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -15,3 +15,6 @@
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
 {
 char c = va_arg(types, int);
+
+return (handle_write_char(c, buffer, flags, width, precision, size));
+}
